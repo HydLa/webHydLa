2586fe381b35a70fa49832d478e00eb4d97ad1c9
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2020-03-30 15:59:43 +0900

    add dynamic draw op

--- test.py
+++ test.py
@@ -29,24 +29,24 @@ def bouncing_particle_op():
   rotate()
   scroll_down()
   time.sleep(1)
-
+  
   # 構造が変わった時は，デベロッパーツール -> Elementsでクリックしたい場所を選んでCopy full XPathで持ってくれば良い
   # show scale labelを2回押す
-  show_scale_label_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[3]/div/div/input"
+  show_scale_label_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[3]"
   driver.find_element_by_xpath(show_scale_label_path).click()
   time.sleep(1)
   driver.find_element_by_xpath(show_scale_label_path).click()
   time.sleep(1)
 
   # 1秒間auto rotate
-  auto_rotate_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[5]/div/div/input"
+  auto_rotate_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[5]"
   driver.find_element_by_xpath(auto_rotate_path).click()
   time.sleep(1)
   driver.find_element_by_xpath(auto_rotate_path).click()
   time.sleep(1)
 
   # XY-modeを2回押す
-  xymode_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[4]/div/div/input"
+  xymode_path = "/html/body/div[2]/div[2]/div[1]/div/ul/li[4]"
   driver.find_element_by_xpath(xymode_path).click()
   time.sleep(1)
   driver.find_element_by_xpath(xymode_path).click()
@@ -64,7 +64,7 @@ def bouncing_particle_op():
   plot_interval.clear()
   plot_interval.send_keys("0.01", Keys.RETURN)
   time.sleep(1)
-
+  
   # line widthを1 -> 2 -> 1と変更
   line_width = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[2]/div/div/div[1]/input")
   line_width.clear()
@@ -74,28 +74,51 @@ def bouncing_particle_op():
   line_width.send_keys("1", Keys.RETURN)
   time.sleep(1)
 
-  # add new line
-  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[8]/div/span").click()
-  time.sleep(1)
-
   # y'を描画
+  add_new_line = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[9]")
+  add_new_line.click()
   # plot1 x 
-  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[9]/div/ul/li[3]/div/ul/li[2]/div/div/input").send_keys("t", Keys.RETURN)
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[2]/div/div/input").send_keys("t", Keys.RETURN)
   # plot1 y
-  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[9]/div/ul/li[3]/div/ul/li[3]/div/div/input").send_keys("y'", Keys.RETURN)
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[3]/div/div/input").send_keys("y'", Keys.RETURN)
   # plot1 z
-  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[9]/div/ul/li[3]/div/ul/li[4]/div/div/input").send_keys("0", Keys.RETURN)
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[4]/div/div/input").send_keys("0", Keys.RETURN)
   time.sleep(1)
   # remove
-  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[9]/div/ul/li[3]/div/ul/li[5]").click()
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[5]").click()
   time.sleep(1)
+  
+  # yとy'を描画
+  z = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[2]/div/ul/li[4]/div/div/input")
+  z.clear()
+  z.send_keys("y'", Keys.RETURN)
+  src = driver.find_element_by_id("scroll-message")
+  ActionChains(driver).drag_and_drop_by_offset(src, -250, 40).release().perform()
+  scroll_down()
+  # dynamic draw
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[6]").click()
+  
+  add_new_line.click()
+  # plot1 x 
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[2]/div/div/input").send_keys("t", Keys.RETURN)
+  # plot1 y
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[3]/div/div/input").send_keys("2*y", Keys.RETURN)
+  # plot1 z
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[4]/div/div/input").send_keys("2*y'", Keys.RETURN)
+  time.sleep(10)
+  # remove
+  driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div/ul/li[10]/div/ul/li[3]/div/ul/li[5]").click()
+  time.sleep(1)
+  line_width.clear()
+  line_width.send_keys("2", Keys.RETURN)
+  time.sleep(10)
 
 if __name__ == '__main__':
   try:
     driver = webdriver.Chrome()
 
     driver.maximize_window()
-    driver.get("http://webhydla.ueda.info.waseda.ac.jp/")
+    driver.get("localhost:5000")
 
     bouncing_particle_op()
   finally:
