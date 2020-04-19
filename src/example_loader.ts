38a283086660382955e673518e0a8a721774d95a
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2020-04-19 20:24:18 +0900

    add

--- src/example_loader.ts
+++ src/example_loader.ts
@@ -2,13 +2,14 @@ export class ExampleLoader {
   static names: string[] = [];
   static contents = new Map<string, string>();
   static readonly path = "https://api.github.com/repos/HydLa/HyLaGI/contents/examples";
+  static readonly auth = "?client_id=026dd130163c7d424aa9&client_secret=413e22c321fbf47c9b7758e3d62193614cc5c6b5"
 
   static init() {
     this.loadExamples();
   }
 
   static loadExamples() {
-    fetch(this.path)
+    fetch(this.path + this.auth)
       .then((res) => res.json())
       .then((json) => { // 例題ディレクトリからHydLaプログラムのファイル名を取得
         for (let f of json) {
@@ -26,6 +27,7 @@ export class ExampleLoader {
           option.value = name;
           select.appendChild(option);
         }
+        (<any>$("#example_selector")).formSelect();
       });
     /*
     .then(() => { // 各HydLaプログラムの中身を取得
