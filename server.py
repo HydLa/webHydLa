9237dd5c59925c9a22dd9e6c33ff5d9df7a0a8da
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2020-10-31 21:27:20 +0900

    refactor

--- server.py
+++ server.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
-from flask import Flask, redirect, url_for, request, abort, jsonify, render_template, session
-import sys, os, time, shlex, subprocess, json, urllib.request, urllib.parse, shutil, datetime
+from flask import Flask, request, abort, jsonify, render_template, session
+import os
+import time
+import shlex
+import subprocess
+import json
+import urllib.request
+import urllib.parse
+import shutil
+import datetime
 
 key_file = open('secret_key')
 # setup Flask
-app = Flask(__name__,static_url_path="")
+app = Flask(__name__, static_url_path="")
 app.secret_key = key_file.read()
 key_file.close()
 
 # defining redirection from '/' to '/index.html'
+
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
-save_dir = "/tmp/webHydLa_" + os.getenv("USER")
+
+save_dir = "/tmp/webHydLa_" + str(os.getenv("USER"))
 hylagi_processes = {}
+
 
 @app.route('/start_session', methods=['GET', 'POST'])
 def start_session():
@@ -30,8 +42,7 @@ def start_session():
 def gen_hydat():
     if request.method == 'GET':
         abort(400)
-        
-    start_time = time.time()
+
     session_id = session['sid']
     save_file_prefix = session_id
     save_file_hydla = save_dir + "/" + save_file_prefix + ".hydla"
@@ -42,13 +53,13 @@ def gen_hydat():
     form = request.form
 
     if "hylagi_option" not in form:
-       return jsonify(sid=session_id, error=1, message="No HyLaGI Option")
+        return jsonify(sid=session_id, error=1, message="No HyLaGI Option")
 
     if "hydla_code" not in form:
-       return jsonify(sid=session_id, error=2, message="No HydLa Code")
+        return jsonify(sid=session_id, error=2, message="No HydLa Code")
 
     if "timeout_option" not in form:
-       return jsonify(sid=session_id, error=3, message="No TimeOut")
+        return jsonify(sid=session_id, error=3, message="No TimeOut")
 
     hylagi_args = ["hylagi"]
 
@@ -68,9 +79,10 @@ def gen_hydat():
         return jsonify(sid=session_id, error=3, message="OSError")
 
     with open(save_file_stdout, "w") as f_stdout, open(save_file_stderr, "w") as f_stderr:
-        if shutil.which("hylagi") != None:
+        if shutil.which("hylagi") is not None:
             hylagi_args.append(save_file_hydla)
-            hylagi_proc = subprocess.Popen(hylagi_args, stdout=f_stdout, stderr=f_stderr)
+            hylagi_proc = subprocess.Popen(
+                hylagi_args, stdout=f_stdout, stderr=f_stderr)
             hylagi_processes[session_id] = hylagi_proc
             try:
                 hylagi_retcode = hylagi_proc.wait(timeout=time_out)
@@ -80,9 +92,8 @@ def gen_hydat():
         else:
             # hylagiがないときは、apiサーバーに投げる
             param_tuple = (
-                ("code", form["hydla_code"])
-                , ("args", hylagi_args)
-                , ("hydat_path", save_file_hydat)
+                ("code", form["hydla_code"]), ("args",
+                                               hylagi_args), ("hydat_path", save_file_hydat)
             )
             encoded_param = urllib.parse.urlencode(param_tuple)
             url = "http://webhydla.ueda.info.waseda.ac.jp:8080?" + encoded_param
@@ -114,15 +125,14 @@ def gen_hydat():
         try:
             with open(save_file_stdout, "r") as f_stdout:
                 if not hylagi_retcode == 0:
-                    return jsonify(sid=session_id, stderr=f_stderr.read(),stdout=f_stdout.read(),error=6,message="An error has occurred in HyLaGI")
+                    return jsonify(sid=session_id, stderr=f_stderr.read(), stdout=f_stdout.read(), error=6, message="An error has occurred in HyLaGI")
                 try:
                     with open(save_file_hydat, "r") as f_hydat:
                         return jsonify(sid=session_id, error=0, message="Simulation was successful", hydat=json.load(f_hydat), stdout=f_stdout.read(), stderr=f_stderr.read())
                 except IOError:
-                    return jsonify(sid=session_id, error=0, message="HyLaGI was successful (but it did not make hydat)", stdout=f_stdout.read(), stderr=f_stderr.read())                
+                    return jsonify(sid=session_id, error=0, message="HyLaGI was successful (but it did not make hydat)", stdout=f_stdout.read(), stderr=f_stderr.read())
         except IOError:
             return jsonify(sid=session_id, error=0, message="HyLaGI was successful (but it did not make output)")
-
 
 
 # defining hylagi killer
@@ -137,12 +147,13 @@ def kill():
         hylagi_processes[session_id].kill()
         hylagi_processes.pop(session_id, None)
     except ProcessLookupError:
-        pass # do nothing
+        pass  # do nothing
     session.pop('sid', None)
     return ""
 
+
 flask_port = 5000
-        
+
 
 # defining error viewer
 @app.route('/error.cgi', methods=['GET', 'POST'])
@@ -160,6 +171,7 @@ def gen_error():
     except IOError:
         abort(500)
 
+
 flask_port = 5000
 
 # run server if this script is running as main program (not import sentence)
@@ -167,7 +179,8 @@ if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
     for error_count in range(100):
         try:
-            app.run(debug=False,host='0.0.0.0',port=flask_port,threaded=True)
+            app.run(debug=False, host='0.0.0.0',
+                    port=flask_port, threaded=True)
             break
         except OSError as e:
             if not e.errno == 98:
@@ -175,5 +188,3 @@ if __name__ == '__main__':
             print(e.strerror)
             flask_port += 1
             continue
-
-
