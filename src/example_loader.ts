6ee156f4b175cb9b387f9c2adc75ca7d5ccb6563
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2021-02-23 14:50:44 +0900

    関数を分割，状態を削除

--- src/example_loader.ts
+++ src/example_loader.ts
@@ -4,71 +4,67 @@ import { EditorControl } from './editor_control';
  * HyLaGIのレポジトリから例題を取得する
  */
 
-const path = 'https://api.github.com/repos/HydLa/HyLaGI/contents/examples';
-
-export interface ExampleLoaderState {
-  names: string[];
-  contents: Map<string, string>;
-}
-
-const exampleLoaderState: ExampleLoaderState = {
-  names: [],
-  contents: new Map<string, string>(),
-};
+const path = new URL('https://api.github.com/repos/HydLa/HyLaGI/contents/examples');
 
 export function initExampleLoader() {
   loadExamples();
 }
 
-function loadExamples() {
-  fetch(path)
-    .then((res) => res.json())
-    .then((json) => {
-      // 例題ディレクトリからHydLaプログラムのファイル名を取得
-      for (const f of json) {
-        if (f.name.indexOf('.hydla') != -1) {
-          exampleLoaderState.names.push(f.name);
-        }
-      }
-    })
-    .then(() => {
-      // 選択可能な例題に取得したファイル名を追加
-      for (const name of exampleLoaderState.names) {
-        const select = <HTMLElement>document.getElementById('example_selector');
-        const option = document.createElement('option');
-        option.text = name;
-        option.value = name;
-        select.appendChild(option);
-      }
-      (<any>$('#example_selector')).formSelect();
-    })
-    .then(() => {
-      // 各HydLaプログラムの中身を取得
-      for (const name of exampleLoaderState.names) {
-        const url = `${path}/${name}`;
-        fetch(url)
-          .then((res) => res.json())
-          .then((json) => {
-            const encoded_content = json.content.replace(/\n/g, '');
-            const content = atob(encoded_content);
-            exampleLoaderState.contents.set(name, content);
-          });
-      }
-    });
+async function loadExamples() {
+  const filenames = await getFilenames();
+
+  // 選択可能な例題に取得したファイル名を追加
+  for (const name of filenames) {
+    const select = <HTMLElement>document.getElementById('example_selector');
+    const option = document.createElement('option');
+    option.text = name;
+    option.value = name;
+    select.appendChild(option);
+  }
+  (<any>$('#example_selector')).formSelect();
 
   document.getElementById('load-examples-button')?.addEventListener('click', () => {
     loadContents();
   });
 }
 
-function loadContents() {
+async function getFilenames() {
+  const filenames: string[] = [];
+
+  const res = await fetch(path.toString());
+  const json = await res.json();
+  // 例題ディレクトリからHydLaプログラムのファイル名を取得
+  for (const f of json) {
+    if (f.name.indexOf('.hydla') != -1) {
+      filenames.push(f.name);
+    }
+  }
+
+  return filenames;
+}
+
+async function loadContents() {
+  const filename = getSelectedFilename();
+  if (filename === null) return;
+  const content = getContent(filename);
+  if (content === undefined) return;
+  EditorControl.editor.setValue(await content);
+}
+
+function getSelectedFilename() {
   const selected = document.getElementsByClassName('selected');
   let fileindex = -1;
   for (let i = 0; i < selected.length; i++)
     if ((<string>selected[i].textContent).indexOf('.hydla') != -1) fileindex = i;
-  if (fileindex == -1) return;
-  const filename = <string>selected[fileindex].textContent;
+  if (fileindex == -1) return null;
+  return selected[fileindex].textContent;
+}
 
-  if (exampleLoaderState.contents.has(filename))
-    EditorControl.editor.setValue(<string>exampleLoaderState.contents.get(filename));
+async function getContent(filename: string) {
+  const url = `${path}/${filename}`;
+  const res = await fetch(url);
+  const json = await res.json();
+  const encoded_content = json.content.replace(/\n/g, '');
+  const content = atob(encoded_content);
+  return content;
 }
