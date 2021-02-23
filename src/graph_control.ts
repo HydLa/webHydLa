b2e58dbcc7b95ce8ca5e30d4bbc3a41b321b05cf
amezaki1007 <mkwamezaki1007@gmail.com>
2021-02-23 18:20:51 +0900

    唯一外部（animation_control）から直接触れられるフィールドface_aの扱いに悩み中

--- src/graph_control.ts
+++ src/graph_control.ts
@@ -10,26 +10,27 @@ import { HydatControl } from './hydat_control';
  * 描画，再描画，クリアなどを行う<br>
  * いわゆるView
  */
+
 export class GraphControl {
-  static scene: THREE.Scene;
-  static camera: THREE.OrthographicCamera;
-  static elem: HTMLElement;
-  static controls: OrbitControls;
-  static renderer: THREE.WebGLRenderer;
-  static animatable = true;
-  static range_mode = false;
+  scene: THREE.Scene;
+  camera: THREE.OrthographicCamera;
+  elem: HTMLElement;
+  controls: OrbitControls;
+  renderer: THREE.WebGLRenderer;
+  animatable = true;
+  range_mode = false;
 
-  static controls_position0: THREE.Vector3;
+  controls_position0: THREE.Vector3;
 
-  static a_line = 1;
-  static t_line = 0;
-  static last_frame_zoom = 1;
+  a_line = 1;
+  t_line = 0;
+  last_frame_zoom = 1;
 
-  static resizeLoopCount = 0;
+  resizeLoopCount = 0;
 
   static face_a: THREE.Mesh[];
 
-  static init() {
+  constructor() {
     this.scene = new THREE.Scene();
 
     // PerspectiveCamera
@@ -65,145 +66,187 @@ export class GraphControl {
 
     //TODO: implement this in more elegant way
     setTimeout(() => {
-      this.resizeGraphRenderer();
+      resizeGraphRenderer();
     }, 200);
   }
+}
 
-  static resizeGraphRenderer() {
-    if (this.elem.clientWidth > 0 && this.elem.clientHeight > 0) {
-      this.renderer.setSize(this.elem.clientWidth, this.elem.clientHeight);
-      const prev_width = this.camera.right - this.camera.left;
-      const prev_height = this.camera.top - this.camera.bottom;
-      let extend_rate;
-      if (prev_width != this.camera.right - this.camera.left) extend_rate = this.elem.clientWidth / prev_width;
-      else extend_rate = this.elem.clientHeight / prev_height;
+const graphControl = new GraphControl();
 
-      this.camera.left = -this.elem.clientWidth / 2;
-      this.camera.right = this.elem.clientWidth / 2;
-      this.camera.top = this.elem.clientHeight / 2;
-      this.camera.bottom = -this.elem.clientHeight / 2;
-      this.camera.zoom *= extend_rate;
+export function initGraphControl() {
+  graphControl.scene = new THREE.Scene();
 
-      this.camera.updateProjectionMatrix();
+  // PerspectiveCamera
+  const width = 50;
+  const height = 50;
 
-      const w = $('#scale_label_wrapper').width()!;
-      const h = $('#scale_label_wrapper').height()!;
-      $('#scaleLabelCanvas').attr('width', w);
-      $('#scaleLabelCanvas').attr('height', h);
-      PlotControl.update_axes(true);
+  // OrthographicCamera
+  const left = width / -2;
+  const right = width / 2;
+  const top = height / 2;
+  const bottom = height / -2;
+  graphControl.camera = new THREE.OrthographicCamera(left, right, top, bottom, -1000, 1000);
 
-      $('#nameLabelCanvas').attr('width', w);
-      $('#nameLabelCanvas').attr('height', h);
-      GraphControl.modifyNameLabel(HydatControl.current_hydat?.name);
-    }
+  graphControl.camera.position.set(0, 0, 100);
+  graphControl.controls_position0 = new THREE.Vector3(0, 0, 100);
+
+  graphControl.elem = document.getElementById('graph-area')!;
+  graphControl.renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  graphControl.renderer.domElement.style.position = 'absolute';
+  graphControl.renderer.domElement.style.width = '100%';
+  graphControl.renderer.domElement.style.height = '100%';
+
+  graphControl.elem.appendChild(graphControl.renderer.domElement);
+
+  const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+  directionalLight.position.set(0, 7, 10);
+
+  graphControl.scene.add(directionalLight);
+
+  graphControl.controls = new OrbitControls(graphControl.camera, graphControl.renderer.domElement);
+  graphControl.controls.screenSpacePanning = true;
+
+  //TODO: implement this in more elegant way
+  setTimeout(() => {
+    resizeGraphRenderer();
+  }, 200);
+}
+
+export function resizeGraphRenderer() {
+  if (graphControl.elem.clientWidth > 0 && graphControl.elem.clientHeight > 0) {
+    graphControl.renderer.setSize(graphControl.elem.clientWidth, graphControl.elem.clientHeight);
+    const prev_width = graphControl.camera.right - graphControl.camera.left;
+    const prev_height = graphControl.camera.top - graphControl.camera.bottom;
+    let extend_rate;
+    if (prev_width != graphControl.camera.right - graphControl.camera.left)
+      extend_rate = graphControl.elem.clientWidth / prev_width;
+    else extend_rate = graphControl.elem.clientHeight / prev_height;
+
+    graphControl.camera.left = -graphControl.elem.clientWidth / 2;
+    graphControl.camera.right = graphControl.elem.clientWidth / 2;
+    graphControl.camera.top = graphControl.elem.clientHeight / 2;
+    graphControl.camera.bottom = -graphControl.elem.clientHeight / 2;
+    graphControl.camera.zoom *= extend_rate;
+
+    graphControl.camera.updateProjectionMatrix();
+
+    const w = $('#scale_label_wrapper').width()!;
+    const h = $('#scale_label_wrapper').height()!;
+    $('#scaleLabelCanvas').attr('width', w);
+    $('#scaleLabelCanvas').attr('height', h);
+    PlotControl.update_axes(true);
+
+    $('#nameLabelCanvas').attr('width', w);
+    $('#nameLabelCanvas').attr('height', h);
+    modifyNameLabel(HydatControl.current_hydat?.name);
   }
+}
 
-  static modifyNameLabel(name: string | undefined) {
-    let text = '';
-    if (!(name == undefined || name == null)) {
-      text = name;
-    }
-    const canvas = <HTMLCanvasElement>document.getElementById('nameLabelCanvas');
-    if (!canvas || !canvas.getContext) {
-      return false;
-    }
-    const ctx = canvas.getContext('2d')!;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "20px 'Arial'";
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillText(text, 0, canvas.height - 50);
+export function modifyNameLabel(name: string | undefined) {
+  let text = '';
+  if (!(name == undefined || name == null)) {
+    text = name;
   }
-
-  static render() {
-    requestAnimationFrame(() => {
-      this.render();
-    });
-    this.controls.update();
-    if (this.last_frame_zoom !== this.camera.zoom) {
-      GraphControl.replotAll();
-    }
-    PlotControl.update_axes(false);
-    if (this.animatable) {
-      AnimationControl.animate(); // animating function
-      AnimationControl.animateTime();
-    } else {
-      AnimationControl.animate();
-    }
-    if (AnimationControl.getLength() !== this.a_line) {
-      if (GraphControl.range_mode) {
-        AnimationControl.range_make_all();
-      }
-      this.a_line = AnimationControl.getLength();
-    }
-    if (AnimationControl.maxlen !== this.t_line) {
-      this.t_line = AnimationControl.maxlen;
-      DatGUIControl.parameter_seek_setting(this.t_line);
-    } else if (this.animatable) {
-      DatGUIControl.parameter_seek_setting_animate(this.t_line, AnimationControl.time);
-    }
-    this.last_frame_zoom = this.camera.zoom;
+  const canvas = <HTMLCanvasElement>document.getElementById('nameLabelCanvas');
+  if (!canvas || !canvas.getContext) {
+    return false;
   }
-
-  static render_three_js() {
-    this.renderer.render(this.scene, this.camera);
+  const ctx = canvas.getContext('2d')!;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "20px 'Arial'";
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillText(text, 0, canvas.height - 50);
+}
+export function renderGraph() {
+  requestAnimationFrame(() => {
+    renderGraph();
+  });
+  graphControl.controls.update();
+  if (graphControl.last_frame_zoom !== graphControl.camera.zoom) {
+    replotAll();
   }
-
-  static toScreenPosition(pos: THREE.Vector3) {
-    const widthHalf = 0.5 * this.renderer.getContext().canvas.width;
-    const heightHalf = 0.5 * this.renderer.getContext().canvas.height;
-
-    pos.project(this.camera);
-
-    return {
-      x: pos.x * widthHalf + widthHalf,
-      y: -(pos.y * heightHalf) + heightHalf,
-    };
+  PlotControl.update_axes(false);
+  if (graphControl.animatable) {
+    AnimationControl.animate(); // animating function
+    AnimationControl.animateTime();
+  } else {
+    AnimationControl.animate();
   }
-
-  static updateRotate(autoRotate: boolean) {
-    this.controls.autoRotate = autoRotate;
-  }
-
-  static update2DMode(twoDimensional: boolean) {
-    this.controls.enableRotate = !twoDimensional;
-    this.controls.mouseButtons = {
-      LEFT: twoDimensional ? THREE.MOUSE.PAN : THREE.MOUSE.ROTATE,
-      MIDDLE: THREE.MOUSE.DOLLY,
-      RIGHT: THREE.MOUSE.PAN,
-    };
-    if (twoDimensional) {
-      this.camera.position.copy(this.controls_position0.clone());
-      this.controls.target.set(0, 0, 0);
-      this.camera.updateMatrix(); // make sure camera's local matrix is updated
-      this.camera.updateMatrixWorld(); // make sure camera's world matrix is updated
+  if (AnimationControl.getLength() !== graphControl.a_line) {
+    if (graphControl.range_mode) {
+      AnimationControl.range_make_all();
     }
+    graphControl.a_line = AnimationControl.getLength();
   }
+  if (AnimationControl.maxlen !== graphControl.t_line) {
+    graphControl.t_line = AnimationControl.maxlen;
+    DatGUIControl.parameter_seek_setting(graphControl.t_line);
+  } else if (graphControl.animatable) {
+    DatGUIControl.parameter_seek_setting_animate(graphControl.t_line, AnimationControl.time);
+  }
+  graphControl.last_frame_zoom = graphControl.camera.zoom;
+}
 
-  static startResizingGraphArea() {
-    this.resizeLoopCount = 0;
+export function renderGraph_three_js() {
+  graphControl.renderer.render(graphControl.scene, graphControl.camera);
+}
+
+export function toScreenPosition(pos: THREE.Vector3) {
+  const widthHalf = 0.5 * graphControl.renderer.getContext().canvas.width;
+  const heightHalf = 0.5 * graphControl.renderer.getContext().canvas.height;
+
+  pos.project(graphControl.camera);
+
+  return {
+    x: pos.x * widthHalf + widthHalf,
+    y: -(pos.y * heightHalf) + heightHalf,
+  };
+}
+
+export function updateRotate(autoRotate: boolean) {
+  graphControl.controls.autoRotate = autoRotate;
+}
+
+export function update2DMode(twoDimensional: boolean) {
+  graphControl.controls.enableRotate = !twoDimensional;
+  graphControl.controls.mouseButtons = {
+    LEFT: twoDimensional ? THREE.MOUSE.PAN : THREE.MOUSE.ROTATE,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.PAN,
+  };
+  if (twoDimensional) {
+    graphControl.camera.position.copy(graphControl.controls_position0.clone());
+    graphControl.controls.target.set(0, 0, 0);
+    graphControl.camera.updateMatrix(); // make sure camera's local matrix is updated
+    graphControl.camera.updateMatrixWorld(); // make sure camera's world matrix is updated
+  }
+}
+
+export function startResizingGraphArea() {
+  graphControl.resizeLoopCount = 0;
+  setTimeout(() => {
+    resizeGraphArea();
+  }, 10);
+}
+
+export function resizeGraphArea() {
+  graphControl.resizeLoopCount++;
+  resizeGraphRenderer();
+  //TODO: do this without timer
+  if (graphControl.resizeLoopCount < 80)
     setTimeout(() => {
-      this.resizeGraphArea();
+      resizeGraphArea();
     }, 10);
-  }
+}
 
-  static resizeGraphArea() {
-    this.resizeLoopCount++;
-    this.resizeGraphRenderer();
-    //TODO: do this without timer
-    if (this.resizeLoopCount < 80)
-      setTimeout(() => {
-        this.resizeGraphArea();
-      }, 10);
-  }
+export function clearPlot() {
+  graphControl.scene = new THREE.Scene();
+  // TODO: 複数のプロットが存在するときの描画範囲について考える
+  // TODO: 設定を変更した時に動的に変更が反映されるようにする
+}
 
-  static clearPlot() {
-    this.scene = new THREE.Scene();
-    // TODO: 複数のプロットが存在するときの描画範囲について考える
-    // TODO: 設定を変更した時に動的に変更が反映されるようにする
-  }
-
-  static replotAll() {
-    PlotLineMapControl.replot();
-    AnimationControl.time = 0;
-  }
+export function replotAll() {
+  PlotLineMapControl.replot();
+  AnimationControl.time = 0;
 }
