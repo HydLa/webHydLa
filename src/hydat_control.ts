0e9b69b7eb47ffd62d766fa988de87a568f95530
sano <70003587+sano-jin@users.noreply.github.com>
2021-02-23 20:19:37 +0900

    Refactor dom control (#45)
    * 関数を外に出した
    
    * 関数（メソッド）を外に出した
    
    * ビルドできるようにはなったはず…
    
    * conflictを解消した（はず…）
    
    * クラスを export しないようにした
    
    Co-authored-by: sano <sano@ueda.info.waseda.ac.jp>

--- src/hydat_control.ts
+++ src/hydat_control.ts
@@ -3,7 +3,7 @@ import { StorageControl } from './storage_control';
 import { GraphControl } from './graph_control';
 import { PlotLineMapControl } from './plot_line_map_control';
 import { PlotControl } from './plot_control';
-import { DOMControl } from './dom_control';
+import { showToast } from './dom_control';
 import { DatGUIControl } from './dat_gui_control';
 
 export class HydatControl {
@@ -59,7 +59,7 @@ export class HydatControl {
     } catch (e) {
       console.log(e);
       console.log(e.stack);
-      DOMControl.showToast('Failed to load hydat: ' + e.name + '(' + e.message + ')', 3000, 'red darken-4');
+      showToast('Failed to load hydat: ' + e.name + '(' + e.message + ')', 3000, 'red darken-4');
     }
     GraphControl.clearPlot();
     if (this.current_hydat !== undefined) {
