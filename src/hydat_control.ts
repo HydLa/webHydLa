3bd0ba6240abdc7215cbb7caff8ebfa80051abcb
ayhsmt97 <ayhsmt97@akane.waseda.ac.jp>
2021-02-24 10:05:46 +0900

    HydatControl クラスを復元、メソッドを分離

--- src/hydat_control.ts
+++ src/hydat_control.ts
@@ -6,9 +6,9 @@ import { PlotControl } from './plot_control';
 import { DOMControl } from './dom_control';
 import { DatGUIControl } from './dat_gui_control';
 
-export interface HydatControl {
-  current_hydat: Hydat | undefined;
-  settingsForCurrentHydat: {
+export class HydatControl {
+  static current_hydat: Hydat | undefined;
+  static settingsForCurrentHydat: {
     plot_line_settings: {
       x: string;
       y: string;
@@ -20,16 +20,13 @@ export interface HydatControl {
 
 export function initHydatControl(saved_hydat: string | null) {
   if (saved_hydat) {
-    loadHydat(JSON.parse(saved_hydat), {
-      current_hydat: undefined,
-      settingsForCurrentHydat: { plot_line_settings: [] },
-    });
+    loadHydat(JSON.parse(saved_hydat));
   }
 }
 
-export function saveHydat(hydatControl: HydatControl) {
-  if (!hydatControl.current_hydat) return;
-  const blob = new Blob([JSON.stringify(hydatControl.current_hydat.raw)]);
+export function saveHydat() {
+  if (!HydatControl.current_hydat) return;
+  const blob = new Blob([JSON.stringify(HydatControl.current_hydat.raw)]);
   const object = window.URL.createObjectURL(blob);
   const d = new Date();
   const date =
@@ -53,85 +50,20 @@ export function saveHydat(hydatControl: HydatControl) {
   a.dispatchEvent(event);
 }
 
-export function loadHydat(hydat: HydatRaw, hydatControl: HydatControl) {
+export function loadHydat(hydat: HydatRaw) {
   try {
     StorageControl.saveHydat(hydat);
-    hydatControl.current_hydat = new Hydat(hydat);
-    DatGUIControl.parameter_setting(hydatControl.current_hydat.parameters);
-    GraphControl.modifyNameLabel(hydatControl.current_hydat.name);
+    HydatControl.current_hydat = new Hydat(hydat);
+    DatGUIControl.parameter_setting(HydatControl.current_hydat.parameters);
+    GraphControl.modifyNameLabel(HydatControl.current_hydat.name);
   } catch (e) {
     console.log(e);
     console.log(e.stack);
     DOMControl.showToast('Failed to load hydat: ' + e.name + '(' + e.message + ')', 3000, 'red darken-4');
   }
   GraphControl.clearPlot();
-  if (hydatControl.current_hydat !== undefined) {
-    PlotLineMapControl.initVariableSelector(hydatControl.current_hydat);
+  if (HydatControl.current_hydat !== undefined) {
+    PlotLineMapControl.initVariableSelector(HydatControl.current_hydat);
   }
   PlotControl.update_axes(true);
 }
-
-/*export class HydatControl {
-  static current_hydat: Hydat | undefined;
-  static settingsForCurrentHydat: {
-    plot_line_settings: {
-      x: string;
-      y: string;
-      z: string;
-      remove: () => void;
-    }[];
-  };
-
-  static init(saved_hydat: string | null) {
-    if (saved_hydat) {
-      this.loadHydat(JSON.parse(saved_hydat));
-    }
-  }
-
-  /* function to save Hydat file */
-/*
-  static saveHydat() {
-    if (!HydatControl.current_hydat) return;
-    const blob = new Blob([JSON.stringify(HydatControl.current_hydat.raw)]);
-    const object = window.URL.createObjectURL(blob);
-    const d = new Date();
-    const date =
-      d.getFullYear() +
-      '-' +
-      d.getMonth() +
-      1 +
-      '-' +
-      d.getDate() +
-      'T' +
-      d.getHours() +
-      '-' +
-      d.getMinutes() +
-      '-' +
-      d.getSeconds();
-    const a = document.createElement('a');
-    a.href = object;
-    a.download = date + '.hydat';
-    const event = document.createEvent('MouseEvents');
-    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(event);
-  }
-
-  static loadHydat(hydat: HydatRaw) {
-    try {
-      StorageControl.saveHydat(hydat);
-      this.current_hydat = new Hydat(hydat);
-      DatGUIControl.parameter_setting(this.current_hydat.parameters);
-      GraphControl.modifyNameLabel(this.current_hydat.name);
-    } catch (e) {
-      console.log(e);
-      console.log(e.stack);
-      DOMControl.showToast('Failed to load hydat: ' + e.name + '(' + e.message + ')', 3000, 'red darken-4');
-    }
-    GraphControl.clearPlot();
-    if (this.current_hydat !== undefined) {
-      PlotLineMapControl.initVariableSelector(this.current_hydat);
-    }
-    PlotControl.update_axes(true);
-  }
-}
-*/
