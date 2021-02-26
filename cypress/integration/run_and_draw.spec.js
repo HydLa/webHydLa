9687b4058aad7e36796181bfd2b9f3573d6876ca
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2021-02-26 15:18:46 +0900

    Revert "UIのチェックを追加 (#65)"
    This reverts commit 5b346fca0ebd7c67976cb1dce6618752fea7bd41.

--- cypress/integration/run_and_draw.spec.js
+++ cypress/integration/run_and_draw.spec.js
@@ -70,9 +70,9 @@ describe('run and draw', () => {
     cy.wait(2000);
 
     cy.contains('add new line').click();
-    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(3) input').type('t');
-    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(4) input').type("y'");
-    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(5) input').type('0{enter}');
+    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(2) input').type('t');
+    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(3) input').type("y'");
+    cy.get('#dat-gui .folder .folder:nth-child(3) li:nth-child(4) input').type('0{enter}');
     cy.wait(1000);
   });
 
@@ -85,28 +85,5 @@ describe('run and draw', () => {
     cy.wait(5000);
     cy.get('#dat-gui > div > ul > li:nth-child(2) input').type('{selectall}{del}3{enter}');
     cy.wait(5000);
-  });
-
-  it('check example and parameter', () => {
-    cy.visit('/');
-
-    cy.contains('Save/Load').click();
-    cy.contains('Examples').click();
-    cy.get('#modal_examples input').click();
-    cy.contains('bouncing_particle_rp.hydla').click();
-    cy.get('#modal_examples').contains('Load').click();
-
-    cy.contains('RUN').click();
-    cy.get('#dat-gui .folder .folder:nth-child(2) li:nth-child(4) input').type('{selectall}{del}ht{enter}');
-    cy.wait(1000);
-
-    cy.contains('fixed').click();
-    cy.wait(1000);
-    cy.contains('range').click();
-    cy.wait(1000);
-    cy.contains('range').click();
-    cy.wait(1000);
-    cy.contains('fixed').click();
-    cy.wait(1000);
   });
 });
