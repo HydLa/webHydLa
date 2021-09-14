027216cd24367d1d6c377e4504df18b6ed1b9055
yuhtota <yuhto.1027@gmail.com>
2021-09-14 15:23:29 +0900

    datGUI.tsにコメントを追加しました

--- src/graph/datGUI.ts
+++ src/graph/datGUI.ts
@@ -6,19 +6,28 @@ import { seekAnimation, removeRanges, makeRanges } from './animation';
 import { setBackgroundColor, updateAxes } from './plot';
 import { HydatParameter, HydatParameterPoint } from '../hydat/hydat';
 
-/** 描画用設定の処理を行う */
+/** 描画用設定の処理を行う(左側のパネル) */
 export class DatGUIState {
+  /** parametersのフォルダ */
   static parameterFolder: dat.GUI;
+  /** variablesのフォルダ */
   static variableFolder: dat.GUI;
+  /** seekのフォルダ */
   static parameterFolderSeek: dat.GUI;
 
+  /** parametersフォルダに追加されていくパラメタ群 */
   static parameterItems: dat.GUIController[] = [];
+  /** valueパネル */
   static parameterItemsSeek: dat.GUIController[] = [];
 
+  /** 描画設定用のデータ構造 */
   static plotSettings: PlotSettings;
 }
 
-/** 初期化 */
+/**
+ * 左側のパネルの初期化
+ * @param plotSettings 描画設定用のデータ構造
+ */
 export function initDatGUIState(plotSettings: PlotSettings) {
   DatGUIState.plotSettings = plotSettings;
   // add new line
@@ -119,6 +128,11 @@ export function initDatGUIState(plotSettings: PlotSettings) {
   fixLayout();
 }
 
+/**
+ * 
+ * @param pars パラメタ群
+ * @returns 
+ */
 export function parameterSetting(pars: Map<string, HydatParameter>) {
   for (const item of DatGUIState.parameterItems) {
     DatGUIState.parameterFolder.remove(item);
@@ -126,16 +140,19 @@ export function parameterSetting(pars: Map<string, HydatParameter>) {
   DatGUIState.parameterItems = [];
   DatGUIState.plotSettings.parameterCondition = new Map();
   for (const [key, par] of pars) {
-    // PPなら終了？
+    // returnじゃなくてcontinue?
     if (par instanceof HydatParameterPoint) return;
 
-    // IPの話？
+    // パラメタの下限と上限
     const lower = par.lowerBound.value.getValue(new Map());
     const upper = par.upperBound.value.getValue(new Map());
+
+    // パラメタの下限と上限の少なくとも1つは有限な値であること
     if (!isFinite(lower) && !isFinite(upper)) {
       throw new Error('Error: at least one of lowerBound and upperBound must be finite.');
     }
 
+    // 下限値が有限でなければ, 上限値-100を下限値とする(上限値も然り)
     const minParValue = isFinite(lower) ? lower : upper - 100;
     const maxParValue = isFinite(upper) ? upper : lower + 100;
     const step = (maxParValue - minParValue) / 100;
@@ -150,16 +167,17 @@ export function parameterSetting(pars: Map<string, HydatParameter>) {
     });
     parameterItem.step(step);
 
-    const modeItem = DatGUIState.parameterFolder.add(DatGUIState.plotSettings.parameterCondition.get(key)!, 'fixed');
+    const modeItemFixed = DatGUIState.parameterFolder.add(DatGUIState.plotSettings.parameterCondition.get(key)!, 'fixed');
     const modeItemRange = DatGUIState.parameterFolder.add(
       DatGUIState.plotSettings.parameterCondition.get(key)!,
       'range'
     );
-    DatGUIState.parameterItems.push(modeItem);
+    DatGUIState.parameterItems.push(modeItemFixed);
     DatGUIState.parameterItems.push(modeItemRange);
     DatGUIState.parameterItems.push(parameterItem);
 
-    modeItem.onChange(function () {
+    // パラメタのfixedのチェックボックス
+    modeItemFixed.onChange(() => {
       if (!DatGUIState.plotSettings.parameterCondition!.get(key)!.fixed) {
         parameterItem.min(1).max(100).step(1).setValue(5);
       } else {
@@ -171,6 +189,7 @@ export function parameterSetting(pars: Map<string, HydatParameter>) {
       }
       replotAll();
     });
+    // パラメタのrangeのチェックボックス
     modeItemRange.onChange(() => {
       graphState.rangeMode = DatGUIState.plotSettings.parameterCondition!.get(key)!.range;
       if (graphState.rangeMode) {
@@ -181,12 +200,17 @@ export function parameterSetting(pars: Map<string, HydatParameter>) {
     });
   }
 
+  // パラメタがあるならparametersフォルダを開いて表示, ないなら閉じておく
   if (pars.size > 0) DatGUIState.parameterFolder.open();
   else DatGUIState.parameterFolder.close();
 
   fixLayout();
 }
 
+/**
+ * valueパネルのシークバーの設定
+ * @param lineLen tの100分の1を1とした数値？
+ */
 export function parameterSeekSetting(lineLen: number) {
   for (const item of DatGUIState.parameterItemsSeek) {
     DatGUIState.parameterFolderSeek.remove(item);
@@ -198,12 +222,14 @@ export function parameterSeekSetting(lineLen: number) {
 
   DatGUIState.plotSettings.parameterConditionSeek = new ParameterConditionSeek(minParValue, maxParValue);
 
+  // seekフォルダにvalueパネルを追加
   const parameterItemSeek = DatGUIState.parameterFolderSeek.add(
     DatGUIState.plotSettings.parameterConditionSeek,
     'value',
     minParValue,
     maxParValue
   );
+  // クリックした時点に飛ぶ
   parameterItemSeek.onChange(() => {
     seekAnimation(DatGUIState.plotSettings.parameterConditionSeek!.value);
   });
@@ -215,7 +241,11 @@ export function parameterSeekSetting(lineLen: number) {
 
   fixLayout();
 }
-
+/**
+ * paramterSeekSettingの引数が1つ増えたやつ(目的は謎)
+ * @param lineLen tの100分の1を1とした数値？
+ * @param timeLine tの100分の1を1とした数値？
+ */
 export function parameterSeekSettingAnimate(lineLen: number, timeLine: number) {
   for (const item of DatGUIState.parameterItemsSeek) {
     DatGUIState.parameterFolderSeek.remove(item);
@@ -247,13 +277,15 @@ export function parameterSeekSettingAnimate(lineLen: number, timeLine: number) {
   fixLayout();
 }
 
+/** to avoid layout collapsion of dat gui */
 export function fixLayout() {
-  // to avoid layout collapsion of dat gui
+  // 数値入力部分の高さ調整
   const dgCInputs = $('.dg .c input[type=text]');
   for (const input of dgCInputs) {
     input.style.height = '100%';
   }
 
+  // グラフエリアの色のセレクター（四角形のやつ）
   const selectors = $('.selector');
   for (const selector of selectors) {
     selector.style.width = '100%';
