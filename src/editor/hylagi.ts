ead78452a8336e167de94979506c0dbd83d3d537
sano <sano@ueda.info.waseda.ac.jp>
2022-03-10 16:19:48 +0900

    toast when http status is not 200

--- src/editor/hylagi.ts
+++ src/editor/hylagi.ts
@@ -81,7 +81,16 @@ export async function sendToHyLaGI(hydla: string) {
     method: 'POST',
     body: form,
   });
-  const responseText = await response.text();
+
+  const res = await response;
+  if (res.status !== 200) {
+    showToast(`Looks like there was a problem. Status Code: ${response.status}`, 3000, 'red darken-4');
+    selectLogTab();
+    console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+    return;
+  }
+
+  const responseText = await res.text();
   console.log(responseText);
   responseHyLaGI(JSON.parse(responseText));
 }
